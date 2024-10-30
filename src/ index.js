f384a04d00aa39715ce85ec57e261656a8537544
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-10-30 12:24:48 +0400

    Add server and app configuration

---
+++ src/ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectToDatabase } from './db/dataConnection';
+
+dotenv.config({
+    path: './.env'
+});
+
+const app = express();
+
+connectToDatabase()
+ .then(() => {
+    const port = process.env.PORT || 3000; // Default port if environment variable is not set
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+ .catch((error) => {
+    console.error("Database connection error:", error);
+    process.exit(1); // Exit the process with a non-zero status code to indicate an error
+  });
+
+app.on('error', (error) => {
+  console.error("Server error:", error);
+  process.exit(1); // Exit the process with a non-zero status code to indicate an error
+});
---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+
+dotenv.config();
+
+const app = express();
+
+app.use(compression())
+
+app.use(bodyParser.json());
+
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true,}));
+
+app.use(express.json({ limit: process.env.Data_Limit }));
+
+app.use(express.urlencoded({extended: true, limit: process.env.Data_Limit}));
+
+app.use(express.static("public"))
+
+app.use(cookieParser());
+
+
+// Routes
+///////////////////////////////////////////////////////////////
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+export {app};
