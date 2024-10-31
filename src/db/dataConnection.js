803ee4a3436a5193c67985d337c2c37be2b661a7
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-10-31 12:06:04 +0400

    Refactor dataConnection.js to remove unnecessary newline at the end of the file

--- src/db/dataConnection.js
+++ src/db/dataConnection.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 
 const connectToDatabase = async (req, res) => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`, );
+    await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
     console.log(`MongoDB Connected !! DB HOST: ${mongoose.connection.host}`);
-
   } catch (error) {
     console.error("MONGO-DB Connection Error:", error);
     process.exit(1);
