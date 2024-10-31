3a7a260ef5dfd0902c8fadde11c14838af26c5c9
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-10-31 12:06:14 +0400

    Refactor ApiError and ApiResponse classes

--- src/utils/JwtToken.js
+++ src/utils/JwtToken.js
@@ -1,18 +1,17 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const generateToken = (user) => {
-
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: '1h',
-    });
-    return token;
-}
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+  return token;
+};
 
 export const options = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    maxAge : 60 * 60, // 1 hour
-    // maxAge : 20 * 1000, // 20 seconds
-    path: '/',
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: "strict",
+  maxAge: 60 * 60, // 1 hour
+  // maxAge : 20 * 1000, // 20 seconds
+  path: "/",
 };
